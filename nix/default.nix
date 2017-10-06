7c159c49765e6f9ecdd2e2aeae9b14be1686ce70
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2017-10-06 14:08:09 +0200

    default.nix and docker.nix builds

--- nix/default.nix
+++ nix/default.nix
@@ -1,15 +1,15 @@
-{ buildPythonPackage,
+{ mkDerivation,
   pykube, effect, tornado, nose, route53, jinja2, python-etcd, boto3, python3
 }:
 let
   version = "1.1";
 in
-buildPythonPackage {
+mkDerivation {
   name = "kuberoute-${version}";
-  propagatedBuildInputs =
-    [ pykube effect tornado nose route53 jinja2 python-etcd boto3 ];
-  buildInputs = [ python3 ];
   src = ../.;
+  propagatedBuildInputs = [
+    pykube effect tornado nose route53 jinja2 python-etcd boto3
+  ];
   checkPhase = ''
     python $src/run-tests
   '';
